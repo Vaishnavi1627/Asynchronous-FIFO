e1f665b9a9474ac2d27bb3f11fccf6ae7d813a43
Vaishnavi1627 <86825272+Vaishnavi1627@users.noreply.github.com>
2021-10-22 09:56:13 +0530

    Update rempty.v

--- rempty.v
+++ rempty.v
@@ -1,40 +1,34 @@
-module rptr_empty #(parameter add_size = 4)
-(
-output reg empty,
-output [add_size-1:0] rd_addr,
-output reg [add_size :0] rd_ptr,
-input [add_size :0] wr_ptr_sync,
-input rd_inc, rd_clk, rd_rst
-);
+module rptr_empty #(parameter add_size = 3)
+(output reg empty,   // empty flag
+output [add_size-1:0] rd_addr,   // read address
+output reg [add_size :0] rd_ptr,  // read pointer
+input [add_size :0] wr_ptr_sync,  // write pointer after synchronization
+input rd_inc, wr_inc ,rd_clk, rd_rst);   // read increment, read clock, read reset
+reg [add_size:0] rbin;  // read binary format
+wire [add_size:0] rgraynext, rbinnext;   // read gray next, read binary next
+wire rempty_val;
 
-reg [add_size:0] rbin;
-wire [add_size:0] rgraynext, rbinnext;
-
-// Memory read-address pointer (okay to use binary to address memory)
-
-assign rd_addr = rbin[add_size-1:0];
+assign rd_addr = rbin[add_size-1:0];  
 assign rbinnext = rbin + (rd_inc & ~empty);
 assign rgraynext = (rbinnext>>1) ^ rbinnext;
 
-// GRAYSTYLE2 pointer
-//-------------------
-always @(posedge rd_clk or negedge rd_rst)
-begin
-if (rd_rst) 
-    {rbin, rd_ptr} <= 0;
-else 
-    {rbin, rd_ptr} <= {rbinnext, rgraynext};
-end  
-//---------------------------------------------------------------
-// FIFO empty when the next rptr == synchronized wptr or on reset
-//---------------------------------------------------------------
-assign rempty_val = (rgraynext == wr_ptr_sync);
+assign rempty_val = ((rd_ptr == wr_ptr_sync) | (rd_inc & (wr_ptr_sync== rgraynext )));  // empty value assignment
 
-always @(posedge rd_clk or negedge rd_rst)
+//	empty <= #1 (wp_s == rp) | (rd_en & (wp_s == rp_pl1));
+always @(posedge rd_clk )
 begin
-if (rd_rst)
-    empty <= 1'b1;
+if (!rd_rst) 
+{rbin, rd_ptr} <= 0;
 else 
-    empty <= rempty_val;
+{rbin, rd_ptr} <= {rbinnext, rgraynext};
+end
+always @(posedge rd_clk )
+begin
+if (!rd_rst) 
+empty <= 1'b1;
+else 
+empty <= rempty_val;
 end
 endmodule
+
+
