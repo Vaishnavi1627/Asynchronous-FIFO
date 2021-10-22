7a2bfcd62e290574642a45c83fe9be9b812884c0
Vaishnavi1627 <86825272+Vaishnavi1627@users.noreply.github.com>
2021-10-22 09:55:01 +0530

    Update mem.v

--- mem.v
+++ mem.v
@@ -1,24 +1,60 @@
-module fifomem #(parameter data_size = 8, // Memory data word width
-parameter add_size = 4) // Number of mem address bits + MSB
+`define VENDOR_FPGA
+//`define VENDOR_XILINX
+//`define VENDOR_ALTERA
 
-(
-output reg [data_size-1:0] data_out,
-input [data_size-1:0] data_in,
-input [add_size-1:0] wr_addr, rd_addr,
-input wr_clken, full, wr_clk, rd_clk
+module generic_dpram(
+	// Generic synchronous dual-port RAM interface
+	rclk, rce, oe, raddr, do,
+	wclk,  wce, wr_en, waddr, di,rd_en,full, empty
 );
 
-localparam DEPTH = 1<< add_size;
-reg [data_size-1:0] mem [0:DEPTH-1];
+	//
+	// Default address and data buses width
+	//
+	parameter aw = 3;  // number of bits in address-bus
+	parameter dw = 8; // number of bits in data-bus
 
-always @(posedge rd_clk)
-begin
-     data_out <= mem[rd_addr];
-end
-always @(posedge wr_clk)
-begin
-if (wr_clken && !full)
-     mem[wr_addr] <= data_in;
-end
+	//
+	// Generic synchronous double-port RAM interface
+	//
+	// read port
+	input           rclk;  // read clock, rising edge trigger	
+	input           rce;   // read port chip enable, active high
+	input           oe;	   // output enable, active high
+	input  [aw-1:0] raddr; // read address
+	output [dw-1:0] do;    // data output
+    input full, empty;
+	// write port
+	input          wclk;  // write clock, rising edge trigger
+	
+	input          wce;   // write port chip enable, active high
+	input          wr_en;    // write enable, active high
+	input          rd_en;
+	input [aw-1:0] waddr; // write address
+	input [dw-1:0] di;    // data input
+
+
+	// Generic dual-port synchronous RAM model
+	
+	// Generic RAM's registers and wires
+	//
+	reg	[dw-1:0]	mem [(1<<aw)-1:0]; // RAM content
+	reg	[dw-1:0]	do_reg;            // RAM data output register
+
+	//
+	// Data output drivers
+	//
+	assign do = (oe & rce) ? do_reg : {dw{1'bz}};
+
+	// read operation
+	always @(posedge rclk)
+		if (rce && rd_en && !empty)
+          		do_reg <=   mem[raddr];
+
+	// write operation
+	always @(posedge wclk)
+		if (wce && wr_en && !full)
+			mem[waddr] <=  di;
+
 
 endmodule
