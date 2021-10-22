f7b585d591a3439449ee22fb21128d8f95371b3a
Vaishnavi1627 <86825272+Vaishnavi1627@users.noreply.github.com>
2021-10-22 09:56:45 +0530

    Update wfull.v

--- wfull.v
+++ wfull.v
@@ -1,42 +1,33 @@
-module wptr_full #(parameter add_size = 4)
+module wptr_full #(parameter add_size = 3)
+(output reg full,
+output [add_size-1:0] wr_addr,  // write address
+output reg [add_size :0] wr_ptr,  //  write pointer
+input [add_size :0] rd_ptr_sync,  // read pointer after synchronization
+input wr_inc, wr_clk, wr_rst);   // write increment, write clock, write reset
+reg [add_size:0] wbin; // write pointer in binary format
+wire [add_size:0] wgraynext, wbinnext;  //write gray next, write binary next
+wire wfull_val;
 
-(
-output reg full,
-output [add_size-1:0] wr_addr,
-output reg [add_size :0] wr_ptr,
-input [add_size :0] rd_ptr_sync,
-input wr_inc, wr_clk, wr_rst
-);
-
-reg  [add_size:0] wbin;
-wire [add_size:0] wgraynext, wbinnext;
-
-
-// Memory write-address pointer (okay to use binary to address memory)
 assign wr_addr = wbin[add_size-1:0];
 assign wbinnext = wbin + (wr_inc & ~full);
 assign wgraynext = (wbinnext>>1) ^ wbinnext;
 
-// GRAYSTYLE2 pointer
-always @(posedge wr_clk or negedge wr_rst)
+assign wfull_val=(((wgraynext[add_size] !=rd_ptr_sync[add_size] ) && (wgraynext[add_size-1] !=rd_ptr_sync[add_size-1]) 
+                    && (wgraynext[add_size-2:0]==rd_ptr_sync[add_size-2:0]) && wr_inc) );  // full flag assignment logic
+ 
+always @(posedge wr_clk or posedge wr_rst)
 begin
-if (wr_rst) 
-    {wbin, wr_ptr} <= 0;
+if (!wr_rst) 
+{wbin, wr_ptr} <= 0;
 else 
-    {wbin, wr_ptr} <= {wbinnext, wgraynext};
+{wbin, wr_ptr} <= {wbinnext, wgraynext};
 end
-assign wfull_val=((wgraynext[add_size] !=rd_ptr_sync[add_size] ) &&
-                  (wgraynext[add_size-1] !=rd_ptr_sync[add_size-1]) &&
-                  (wgraynext[add_size-2:0]==rd_ptr_sync[add_size-2:0]));
-//------------------------------------------------------------------
-//assign wfull_val = (wgraynext=={~rd_ptr_sync[add_size:add_size-1],
-//rd_ptr_sync[add_size-2:0]});
-       
-always @(posedge wr_clk or negedge wr_rst)
+
+always @(posedge wr_clk or posedge wr_rst)
 begin
-if (wr_rst) 
-    full <= 1'b0;
+if (!wr_rst) 
+full <= 1'b0;
 else 
-    full <= wfull_val;
+full <= wfull_val;
 end
 endmodule
